e211652117075af4dd482de0b2f2d455bc9c1287
Rodrigo Durán <rdurandiez@gmail.com>
2021-11-23 19:24:22 +0100

    comit inicial del proyecto Arkanoid. la bola rebota casi bien. Los ladrillos desaparecen. faltan las imágenes el cursor que funcione bien

---
+++ src/codigo/Bola.java
@@ -0,0 +1,68 @@
+package codigo;
+
+import java.awt.Color;
+
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+
+public class Bola extends GOval{
+	int dx = 1; //velocidad en el eje x
+	int dy = 1; //velocidad en el eje y
+	
+	public Bola(double width, double height) {
+		super(width, height);
+		
+	}
+	public Bola(double width, double height, Color c) {
+		super(width, height);
+		this.setFillColor(c);
+		this.setFilled(true);
+	}
+	public void muevete(Arkanoid ark){
+		//rebota con el suelo y techo
+		if (this.getY() > ark.getHeight()|| getY() < 0){
+			dy = dy * -1;
+		}
+		
+		//rebota con la pared de la derecha y izquierda
+		if ( this.getX() > ark.getWidth()|| getX() < 0){
+			dx = dx * -1;
+		}
+		//chequeo la esquina superior izquierda
+		 if (chequeaColision(getX (), getY(),ark)){
+			 if (chequeaColision(getX ()+ getWidth(), getY(),ark)){
+				 if (chequeaColision(getX (), getY()+ getHeight(),ark)){
+					 if (chequeaColision(getX ()+ getWidth(), getY()+ getHeight(),ark)){
+						 
+					 }
+				 }
+			 }
+		 }
+
+		
+		//mueve la bola en la direccion correcta
+		this.move(dx,dy);
+	}
+	private boolean chequeaColision(double posx, double posy,Arkanoid ark){
+		boolean noHaChocado = true;
+		GObject auxiliar;
+		
+		auxiliar = ark.getElementAt(posx,posy);
+		
+		if (auxiliar == ark.miCursor){// si entra aquí es que choca con el cursor
+			dy = dy * -1;
+			noHaChocado = false;
+		}else if (auxiliar == null){// si vale null es que no había nada ahí
+			
+		}else {//suponemos que es un ladrillo
+			ark.remove(auxiliar);
+			dy = dy * -1;
+			dx = dx * -1;
+			noHaChocado = false;
+		}
+		
+		
+		return noHaChocado;
+	}
+	
+}
